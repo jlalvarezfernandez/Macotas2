e6637c1a283cdcb15825ef4097c896788f897ed0
jlalvarezfernandez <jl.alvarezfernandez@hotmail.com>
2021-04-08 15:48:21 +0200

    add styles fundacion

--- cpt/fundacion.php
+++ cpt/fundacion.php
@@ -72,3 +72,12 @@ function up_get_mascotas_fundacion($id){
     ));
     return $adoptados;
 }
+
+function numero_fundaciones( $query ){
+    if( ! is_admin()
+        && $query->is_post_type_archive( 'fundacion' )
+        && $query->is_main_query() ){
+            $query->set( 'posts_per_page', -1 );
+    }
+}
+add_action( 'pre_get_posts', 'numero_fundaciones' );
