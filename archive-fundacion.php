26faf5d42f4d7e88d59e5552caa35e107e92a428
rodrigo-kaferocks <rodrigo@kafe.rocks>
2021-04-09 18:18:56 -0400

    Changed archive-fundacion

--- archive-fundacion.php
+++ archive-fundacion.php
@@ -1,11 +1,17 @@
 <?php get_header(); ?>
-<div class="row-adopciones">
+<div class="row-container-adopciones">
+	<div class="row">
+		<div class="col-md-12">
+		<h2>QR Mascotas colabora con las siguientes fundaciones:</h2>
+		<p></p>
+		</div>
+	</div>
+	<div class="row-adopciones">
 	<?php 
 		if(have_posts()) : while(have_posts()) : the_post(); ?>
 			<!-- <div class="col-xs-12 col-md-4 col-lg-4"> -->
-				<div class="logo">
-					
-					<a href="<?= the_permalink();?>"><?php the_post_thumbnail('full', array( 'class' => 'alignleft')); ?></a>
+				<div class="logo">					
+					<a href="<?= the_permalink();?>"><?php the_post_thumbnail('full', array( 'class' => 'img-fluid')); ?></a>
 				</div>
 				<!-- <div class="title">
 					<h3><?php the_title(); ?></h3>
@@ -19,6 +25,7 @@
 			<!-- </div> --><?php
 	endwhile; endif;
 	?>
+	</div>
 </div>
 <?php get_footer();
 
